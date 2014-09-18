2ff11127badcbae9c4e28a57fdd917f900236cc7
ANIL NARISETTI <anilmail@gmail.com>
2014-09-18 13:20:22 -0700

    First

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojureintro
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojureintro/core.clj
@@ -0,0 +1,235 @@
+(ns clojureintro.core
+  (:use bakery.core))
+
+(defn error [& args]
+  (apply println args)
+  :error
+  )
+
+(defn add-egg []
+  (grab :egg)
+  (squeeze)
+  (add-to-bowl))
+
+(defn add-sugar []
+  (grab :cup)
+  (scoop :sugar)
+  (add-to-bowl)
+  (release))
+
+(defn add-flour []
+  (grab :cup)
+  (scoop :flour)
+  (add-to-bowl)
+  (release))
+
+(defn add-milk []
+  (grab :cup)
+  (scoop :milk)
+  (add-to-bowl)
+  (release))
+
+(defn add-butter []
+  (grab :butter)
+  (add-to-bowl))
+
+(defn add-eggs [n]
+  (dotimes [e n]
+    (add-egg)))
+
+(defn add-flour-cups [n]
+  (dotimes [e n]
+    (add-flour)))
+
+(defn add-milk-cups [n]
+  (dotimes [e n]
+    (add-milk)))
+
+(defn add-sugar-cups [n]
+  (dotimes [e n]
+    (add-sugar)))
+
+(defn add-butters [n]
+  (dotimes [e n]
+    (add-butter)))
+
+(def scooped-ingredients #{:milk :sugar :flour})
+(defn scooped? [ingredient]
+  (contains? scooped-ingredients ingredient))
+
+(def squeezed-ingredients #{:egg})
+(defn squeezed? [ingredient]
+  (contains? squeezed-ingredients ingredient))
+
+(def simple-ingredients #{:butter})
+(defn simple? [ingredient]
+  (contains? simple-ingredients ingredient))
+
+(def pantry-ingredients #{:flour :sugar})
+(defn from-pantry? [ingredient]
+  (contains? pantry-ingredients ingredient))
+
+(def fridge-ingredients #{:milk :egg :butter})
+(defn from-fridge? [ingredient]
+  (contains? fridge-ingredients ingredient))
+
+(defn add-squeezed
+  ([ingredient]
+     (add-squeezed ingredient 1))
+  ([ingredient amount]
+     (if (squeezed? ingredient)
+       (dotimes [i amount]
+         (grab ingredient)
+         (squeeze)
+         (add-to-bowl) :ok)
+       (error "This function only works on squeezed ingredients. You asked me to squeeze" ingredient))))
+
+(defn add-scooped
+  ([ingredient]
+     (add-scooped ingredient 1))
+  ([ingredient amount]
+     (if (scooped? ingredient)
+       (do
+         (grab :cup)
+         (dotimes [i amount]
+          (scoop ingredient)
+          (add-to-bowl))
+          (release) :ok)
+       (error "This function only works on scooped ingredients. You asked me to scoop" ingredient))))
+
+(defn add-simple
+  ([ingredient]
+     (add-simple ingredient 1))
+  ([ingredient amount]
+     (if (simple? ingredient)
+       (dotimes [i amount]
+         (grab ingredient)
+         (add-to-bowl) :ok)
+       (error "This function only works on simple ingredients. You asked me to add" ingredient))))
+
+(defn add
+  ([ingredient]
+     (add ingredient 1))
+  ([ingredient amount]
+     (cond
+      (squeezed? ingredient)
+      (add-squeezed ingredient amount)
+
+      (simple? ingredient)
+      (add-simple ingredient amount)
+
+      (scooped? ingredient)
+      (add-scooped ingredient amount)
+
+      :else
+
+      (error "I do not have the ingredient" ingredient))))
+
+(defn bake-cake []
+  (add :egg 2)
+  (add :flour 2)
+  (add :milk 1)
+  (add :sugar 1)
+
+  (mix)
+
+  (pour-into-pan)
+  (bake-pan 25)
+  (cool-pan))
+
+(defn bake-cookies []
+  (add :egg 1)
+  (add :flour 1)
+  (add :butter 1)
+  (add :sugar 1)
+
+  (mix)
+
+  (pour-into-pan)
+  (bake-pan 30)
+  (cool-pan))
+
+(defn fetch-from-pantry ( [ingredient]
+                          fetch-from-pantry ingredient 1 ) 
+  ([ingredient amount]
+   (if (from-pantry? ingredient)
+     (do
+       (go-to :pantry)
+       (dotimes [i amount]
+         (load-up ingredient))
+       (go-to :prep-area)
+       (dotimes [i amount] 
+         (unload ingredient)))
+     (error "This function only works on ingredients in the pantry. You asked me to fetch" ingredient))))
+
+(defn fetch-from-fridge ( [ingredient]
+                          fetch-from-fridge ingredient 1 ) 
+  ([ingredient amount]
+   (if (from-fridge? ingredient)
+     (do
+       (go-to :fridge)
+       (dotimes [i amount]
+         (load-up ingredient))
+       (go-to :prep-area)
+       (dotimes [i amount] 
+         (unload ingredient)))
+     (error "This function only works on ingredients in the fridge. You asked me to fetch" ingredient))))
+
+(defn fetch-ingredient ([ingredient]
+                          fetch-ingredient ingredient 1)
+  ([ingredient amount]
+      (cond
+       (from-fridge? ingredient)
+       (fetch-from-fridge ingredient amount)
+       (from-pantry? ingredient)
+       (fetch-from-pantry ingredient amount)
+       :else
+       (error "I don't know where to get" ingredient))))
+
+(defn load-up-amount [ingredient amount]
+  (dotimes [i amount]
+    (load-up ingredient)))
+
+(defn unload-amount [ingredient amount]
+  (dotimes [i amount]
+    (unload ingredient)))
+
+(defn fetch-list [shopping-list]
+  (doseq [[location ingredients]
+           {:pantry pantry-ingredients
+            :fridge fridge-ingredients}]
+    (go-to location)
+    (doseq  [ingredient ingredients]
+      (load-up-amount ingredient (ingredient shopping-list 0)))
+
+    (go-to :prep-area)
+    (doseq [ingredient ingredients]
+      (unload-amount ingredient (ingredient shopping-list 0)))))
+
+(def cake-list {:egg 2
+                   :flour 2
+                   :milk 1
+                   :sugar 1})
+
+ (def cookies-list {:egg 1
+                   :flour 1
+                   :butter 1
+                    :sugar 1 })
+
+(defn total-ingredient-list [list f]
+(into {} (for [[k v] list] [k (f v)])))
+
+(defn day-at-the-bakery []
+  (doseq [order (get-morning-orders)]
+    (fetch-list
+     (merge-with + (total-ingredient-list  cake-list #(* (:cake (:items order) 0) %))
+       (total-ingredient-list  cookies-list #(* (:cookies (:items order) 0) %)))
+     (dotimes [i (:cake (:items order) 0)] (bake-cake))
+     (dotimes [i (:cookies (:items order) 0)] (bake-cookies))
+    )))
+                          
+    
+(defn -main []
+  (start-over)
+  (day-at-the-bakery)
+ )
